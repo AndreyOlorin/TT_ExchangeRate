66e591c643dab10eda91add51266e87401e4afd7
AndreyOlorin <sap7andy@gmail.com>
2024-12-25 21:36:46 +0700

    Initial commit

---
+++ app/src/main/java/com/example/tt_exchangerate/MainActivity.kt
@@ -0,0 +1,21 @@
+package com.example.tt_exchangerate
+
+import android.app.ProgressDialog
+import android.content.Intent
+import android.os.AsyncTask
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+
+
+    }
+
+}
