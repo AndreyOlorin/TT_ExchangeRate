2c5a6d6483d9b5a004c67c611315e7f242221b28
AndreyOlorin <sap7andy@gmail.com>
2024-12-26 22:14:01 +0700

    New

--- app/src/main/java/com/example/tt_exchangerate/MainActivity.kt
+++ app/src/main/java/com/example/tt_exchangerate/MainActivity.kt
@@ -5,11 +5,19 @@ import android.content.Intent
 import android.os.AsyncTask
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.RecyclerView
 import kotlinx.android.synthetic.main.activity_main.button_To_Conversion
 import kotlinx.android.synthetic.main.activity_main.button_To_Home
+import org.json.JSONObject
+import java.net.HttpURLConnection
+import java.net.URL
 
 
 class MainActivity : AppCompatActivity() {
+
+
+    lateinit var pDialog: ProgressDialog
+    val list = ArrayList<DataCourse>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -19,11 +27,19 @@ class MainActivity : AppCompatActivity() {
 
         button_To_Home.setBackgroundResource(R.drawable.home_in)
         button_To_Conversion.setBackgroundResource(R.drawable.conversion)
+        AsyncTaskHandler().execute(url)
 
         button_To_Conversion.setOnClickListener{
             val Intent = Intent(this, Conversion::class.java)
             startActivity(Intent)
         }
+
+
+
+    }
+
+    inner class AsyncTaskHandler : AsyncTask<String,String,String>(){
+
 
 
 
