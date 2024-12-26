9ac8c205be5a40c9b8e3586fac13f8822283cd43
AndreyOlorin <sap7andy@gmail.com>
2024-12-26 22:40:21 +0700

    New

--- app/src/main/java/com/example/tt_exchangerate/Conversion.kt
+++ app/src/main/java/com/example/tt_exchangerate/Conversion.kt
@@ -12,5 +12,32 @@ class Conversion : AppCompatActivity() {
 
         button_Conversion.setBackgroundResource(R.drawable.conversion_in)
         button_Home.setBackgroundResource(R.drawable.home)
+
+
+
+        val info = intent.extras
+        val abbreviation :String = info!!.get("abbreviation") as String
+
+        val a = abbreviation.substringAfter("abbreviationName=")
+        val abbreviationBuffer = a.substring(0..2)
+
+        val b = abbreviation.substringAfter("course_nowNumber=")
+        val countBuffer = b.substring(0..4)
+
+        abbreviationTV.text = abbreviationBuffer
+        out_count.text = "Текущий курс :$countBuffer"
+
+        button_convert.setOnClickListener {
+            val buffer_in = in_count.text.toString()
+            val a = buffer_in.toDouble()
+            val c = countBuffer.toDouble()
+            val result = c*a
+            out_count.text = result.toString()
+        }
+
+        button_Home.setOnClickListener {
+            val Intent = Intent(this,MainActivity::class.java)
+            startActivity(Intent)
+        }
     }
 }
