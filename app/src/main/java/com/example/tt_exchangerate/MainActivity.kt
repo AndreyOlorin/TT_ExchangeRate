45530c9bd60051ecca8a88a4af7390e7c4bdd39a
AndreyOlorin <sap7andy@gmail.com>
2024-12-26 22:07:57 +0700

    New

--- app/src/main/java/com/example/tt_exchangerate/MainActivity.kt
+++ app/src/main/java/com/example/tt_exchangerate/MainActivity.kt
@@ -5,6 +5,8 @@ import android.content.Intent
 import android.os.AsyncTask
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.button_To_Conversion
+import kotlinx.android.synthetic.main.activity_main.button_To_Home
 
 
 class MainActivity : AppCompatActivity() {
@@ -13,6 +15,10 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        val url = "https://www.cbr-xml-daily.ru/daily_json.js"
+
+        button_To_Home.setBackgroundResource(R.drawable.home_in)
+        button_To_Conversion.setBackgroundResource(R.drawable.conversion)
 
 
 
