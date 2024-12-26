55115b075cc59fe7fd221f129d03d42c7354eb2b
AndreyOlorin <sap7andy@gmail.com>
2024-12-26 22:47:00 +0700

    New

--- app/src/main/java/com/example/tt_exchangerate/Adapter.kt
+++ app/src/main/java/com/example/tt_exchangerate/Adapter.kt
@@ -9,20 +9,20 @@ import androidx.recyclerview.widget.RecyclerView
 class Adapter (
 
     Context: Context, private val course : List<DataCourse>,
-    private val onClickListener: CourseOnClickListener  ):  RecyclerView.Adapter<Adapter.ViewHolder>() {
+    private val onClickListener: CourseOnClickListener  ):
+    RecyclerView.Adapter<Adapter.ViewHolder>() {
 
-    private val inflater : LayoutInflater = LayoutInflater.from(Context)
+    private val inflater: LayoutInflater = LayoutInflater.from(Context)
     override fun getItemCount(): Int = course.size
-    private fun getItem(position: Int):DataCourse = course[position]
+    private fun getItem(position: Int): DataCourse = course[position]
 
-    class ViewHolder (itemView : View)
-        : RecyclerView.ViewHolder(itemView) {
-        private val abbreviation : TextView = itemView.findViewById(R.id.abbreviation)
-        private val decryption : TextView = itemView.findViewById(R.id.decryption)
-        private val course_now : TextView = itemView.findViewById(R.id.course_now)
-        private val course_last : TextView = itemView.findViewById(R.id.course_last)
+    class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        private val abbreviation: TextView = itemView.findViewById(R.id.abbreviation)
+        private val decryption: TextView = itemView.findViewById(R.id.decryption)
+        private val course_now: TextView = itemView.findViewById(R.id.course_now)
+        private val course_last: TextView = itemView.findViewById(R.id.course_last)
 
-        fun bind (version: DataCourse){
+        fun bind(version: DataCourse) {
             abbreviation.text = version.abbreviationName
             decryption.text = version.decryptionName
             course_now.text = version.course_nowNumber.toString()
@@ -30,4 +30,12 @@ class Adapter (
         }
     }
 
-)
+
+    override fun onBindViewHolder(holder: Adapter.ViewHolder, position: Int) {
+        holder.bind(getItem(position))
+        holder.itemView.setOnClickListener {
+            onClickListener.onClick(position)
+        }
+    }
+
+}
