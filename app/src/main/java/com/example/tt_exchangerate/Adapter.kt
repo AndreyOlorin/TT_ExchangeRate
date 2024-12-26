020ac554c3c82bbfc69b6ea8f968829abe64b6dc
AndreyOlorin <sap7andy@gmail.com>
2024-12-26 22:36:16 +0700

    New

--- app/src/main/java/com/example/tt_exchangerate/Adapter.kt
+++ app/src/main/java/com/example/tt_exchangerate/Adapter.kt
@@ -1,8 +1,33 @@
 package com.example.tt_exchangerate
 
 import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
 
 class Adapter (
 
+    Context: Context, private val course : List<DataCourse>,
+    private val onClickListener: CourseOnClickListener  ):  RecyclerView.Adapter<Adapter.ViewHolder>() {
+
+    private val inflater : LayoutInflater = LayoutInflater.from(Context)
+    override fun getItemCount(): Int = course.size
+    private fun getItem(position: Int):DataCourse = course[position]
+
+    class ViewHolder (itemView : View)
+        : RecyclerView.ViewHolder(itemView) {
+        private val abbreviation : TextView = itemView.findViewById(R.id.abbreviation)
+        private val decryption : TextView = itemView.findViewById(R.id.decryption)
+        private val course_now : TextView = itemView.findViewById(R.id.course_now)
+        private val course_last : TextView = itemView.findViewById(R.id.course_last)
+
+        fun bind (version: DataCourse){
+            abbreviation.text = version.abbreviationName
+            decryption.text = version.decryptionName
+            course_now.text = version.course_nowNumber.toString()
+            course_last.text = version.course_lastNumber.toString()
+        }
+    }
 
 )
